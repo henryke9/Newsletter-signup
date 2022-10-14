8e4b4caa41004cd8c3abb2035dc92ad810c72a18
Henrique <benfi.henry@gmail.com>
2022-10-14 10:43:08 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const request = require("express");
+const bodyParser = require("body-parser")
+const https= require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.get("/", function(req, res) {
+
+  res.sendFile(__dirname + "/signup.html");
+
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName,
+        }
+      }
+    ]
+  };
+
+  var jsonData =JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/4188351232";
+
+  const options = {
+    method: "POST",
+    auth: "henrique:733f5f695a5f9895a331fc42adfb919b-us21"
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if(response.statusCode === 200) {
+      res.send("Welcome to the list");
+    } else {
+      res.send("try again");
+    }
+
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+})
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
+});
+
+
+//API key
+//733f5f695a5f9895a331fc42adfb919b-us21
+
+//4188351232
